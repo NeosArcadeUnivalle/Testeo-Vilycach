a40637ac9f4c68dcabf0b674ea801e4fe3a2295f
NeosArcadeUnivalle <vgg2026342@est.univalle.edu>
2024-11-26 22:06:47 -0400

    Estructura de cada Test (Cliente)

---
+++ test_cases/Cliente/test_compra.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+class testCompra:
+    def setup_method(self):
+        self.driver = webdriver.Edge();
+        self.driver.maximize_window()
+        self.driver.get('http://127.0.0.1:8000/')
+        time.sleep(3)
+
+    def teardown_method(self):
+        self.driver.quit()
+        print("Prueba Completada")
